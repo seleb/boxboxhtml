29ff005b7a405661f013247c9387c4ef681e9d8b
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-09-16 23:04:11 -0400

    corner gizmos

--- src/components/App/GizmoScale.jsx
+++ src/components/App/GizmoScale.jsx
@@ -8,10 +8,16 @@ export default function GizmoScale({
 }) {
 	return (
 		<div class="gizmo-group gizmo-scale">
+			{/* edges */}
 			<GizmoAnchor boxId={id} anchors={['top']} />
 			<GizmoAnchor boxId={id} anchors={['bottom']} />
 			<GizmoAnchor boxId={id} anchors={['left']} />
 			<GizmoAnchor boxId={id} anchors={['right']} />
+			{/* corners */}
+			<GizmoAnchor boxId={id} anchors={['top','left']} />
+			<GizmoAnchor boxId={id} anchors={['bottom','left']} />
+			<GizmoAnchor boxId={id} anchors={['top','right']} />
+			<GizmoAnchor boxId={id} anchors={['bottom','right']} />
 		</div>
 	);
 }
